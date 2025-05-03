c42a1195132e233d91dd57d1c9a7867fc207c939
Rose <rose123@example.com>
2025-05-03 16:50:48 +0800

    Move static files to public folder

--- server.js
+++ server.js
@@ -65,6 +65,11 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'bookForm.html'));
 });
 
+// 顯示 login.html 頁面
+app.get('/login.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
 // 啟動伺服器
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
